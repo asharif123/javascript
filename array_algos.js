7db95598c6f1dc1d8ed607e21e76afe17f10a5fd
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-09-01 15:48:26 -0700

    Add files via upload

---
+++ array_algos.js
@@ -0,0 +1,16 @@
+// arrays + algos
+// Print Values and Sum
+
+// Print each array value and the sum so far
+// 
+
+var testArr = [6,3,5,1,2,4]
+var sum = 0
+for (i = 0; i < testArr.length; i++) {
+    sum += testArr[i]
+    console.log("Num", testArr[i], "Sum", sum)
+}
+
+for (i = 0; i < testArr.length; i++) {
+    console.log(i*testArr[i])
+}

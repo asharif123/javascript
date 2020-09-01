e7f3be2bd31ff896e368b62fa94ba019daa4f3e8
asharif123 <49471791+asharif123@users.noreply.github.com>
2020-09-01 15:05:53 -0700

    Add files via upload

---
+++ javascript_basics.js
@@ -0,0 +1,20 @@
+
+// print 15
+var x = 15;
+console.log(x);
+
+// print addition
+var x = 3;
+x = x + 1
+console.log(x)
+
+var x = 3;
+x = x + 2
+console.log(x)
+console.log(x+2)
+
+// multiplication
+var x = 3
+x = x*x;
+console.log(x)
+console.log(x*2)

812e60ed120ca232c3e46e572b0740ff45b0afa9
= <awadsharif9@gmail.com>
2020-09-14 15:42:52 -0700

    added prompt for user to enter age

---
+++ home.js
@@ -0,0 +1,15 @@
+console.log('hello')
+alert('yo')
+
+// variables
+var b =  'Hello world'
+console.log(b)
+
+// change code on html using javascript
+
+
+
+var age = prompt('Enter Age');
+
+document.getElementById('someText').innerHTML = age;
+
